26bbc39922cbea52ee8d9ee2f9bbd4f98b36325b
John Bridges <jbridges373@gmail.com>
2022-07-24 16:06:08 +0100

    test commit

---
+++ src/constants/data.js
@@ -0,0 +1,38 @@
+export const properties = [
+  {
+    id: 1,
+    imageUrl:
+      "https://blog.dupontregistry.com/wp-content/uploads/2020/08/Parkland_1st-Photo-scaled.jpg",
+    price: "£1,569,000",
+    address: "69 Fake Address",
+    description:
+      "Beautiful house! Offers you 5 bedrooms, 3 full bathrooms with separate showers. Lots of windows and natural light. The finished basement includes a bedroom, a large family room and a full bathroom. Great location! Close to daycares, schools and accessible by public transport. 5 minutes by car from the downtown area. Quick access to highway and subway. On the second floor: The master bedroom is spacious. It has a balcony, a large walk-in closet, and its own en-suite bathroom. The other three bedrooms are of good size and each has its own walk-in closet. The basement is fully finished and offers a family room, as well as a game room. The basement bedroom is convenient with a full bathroom.",
+    numBedroom: 5,
+    numWashrooms: 3,
+    livingSpace: "3,000 sq.ft.",
+  },
+  {
+    id: 2,
+    imageUrl:
+      "https://blog.dupontregistry.com/wp-content/uploads/2021/02/IMP1-scaled.jpg",
+    price: "£2,600,000",
+    address: "187 Fake Address",
+    description:
+      "Beautiful house! Offers you 6 bedrooms, 3 full bathrooms with separate showers. Lots of windows and natural light. The finished basement includes a bedroom, a large family room and a full bathroom. Great location! Close to daycares, schools and accessible by public transport. 5 minutes by car from the downtown area. Quick access to highway and subway. On the second floor: The master bedroom is spacious. It has a balcony, a large walk-in closet, and its own en-suite bathroom. The other three bedrooms are of good size and each has its own walk-in closet. The basement is fully finished and offers a family room, as well as a game room. The basement bedroom is convenient with a full bathroom.",
+    numBedroom: 6,
+    numWashrooms: 3,
+    livingSpace: "2,410 sq.ft.",
+  },
+  {
+    id: 3,
+    imageUrl:
+      "https://ca-times.brightspotcdn.com/dims4/default/c12c0dd/2147483647/strip/true/crop/2000x1195+0+0/resize/1486x888!/quality/90/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2Fad%2Ff4%2F1f1b2193479eafb7cbba65691184%2F10480-sunset-fullres-01.jpg",
+    price: "£3,500,000",
+    address: "373 Fake Address",
+    description:
+      "Beautiful house! Offers you 7 bedrooms, 3 full bathrooms with separate showers. Lots of windows and natural light. The finished basement includes a bedroom, a large family room and a full bathroom. Great location! Close to daycares, schools and accessible by public transport. 5 minutes by car from the downtown area. Quick access to highway and subway. On the second floor: The master bedroom is spacious. It has a balcony, a large walk-in closet, and its own en-suite bathroom. The other three bedrooms are of good size and each has its own walk-in closet. The basement is fully finished and offers a family room, as well as a game room. The basement bedroom is convenient with a full bathroom.",
+    numBedroom: 7,
+    numWashrooms: 3,
+    livingSpace: "4,450 sq.ft.",
+  },
+];
